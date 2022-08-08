0e8ceef65a6bbfefdcf676a6511322b39bdf5bcf
Markus Schmidt <markus262@web.de>
2022-08-08 18:45:01 +0200

    add cell descr not at the desired place

--- Assets/ChartAxis.cs
+++ Assets/ChartAxis.cs
@@ -7,10 +7,13 @@ using TMPro;
 public class ChartAxis : MonoBehaviour
 {
 
+	public GameObject myPrefab; 
+	 GameObject[] axisDescr; 
+
     // Start is called before the first frame update
     void Start()
     {
-	
+	axisDescr = new GameObject[0];
     }
 
     // Update is called once per frame
@@ -21,30 +24,48 @@ public class ChartAxis : MonoBehaviour
 
 
     public void UpdateAxis(string title, List<string> cellDescriptions){
-    
-/*
-    	
+        	
     	// cleanup
-    	for(int cx = 0; cx < axisDescr.GetLength(0); cx++) {
-         for(int cy = 0; cy < axisDescr.GetLength(1); cy++) {
-            for(int cz = 0; cz < axisDescr.GetLength(2); cz++) {
-                Destroy(axisDescr[cx,cy,cz]);
-            }
-         }
-        }
-*/
-    	// TODO: draw texts of cells
-    	
-    	// axisDescr = new GameObject[,,]; 
-    	// for ... GameObject.CreatePrimitive(PrimitiveType.Cube);
+          for(int i = 0; i < axisDescr.GetLength(0); i++) {
+                Destroy(axisDescr[i]);
+          }
+          
+	
+	// draw axis title
 	name = "axis_"+ title;
     	    
 	TMP_Text[] t = GetComponentsInChildren<TMP_Text>();
 	t[0].SetText(title);
+	
+	// draw texts of cells
+	axisDescr = new GameObject[ cellDescriptions.Count ];
+	float height = cellDescriptions.Count*1.1f/2;
+	
+	
+	// find holder
+	Transform descrHolder = null;
+	foreach (Transform child in transform)
+        {
+            if(string.Equals(child.name, "cellDescrHolder")){
+		descrHolder = child;
+            }
+        }
 
-
-//	t.transform.localEulerAngles += new Vector3(90, 0, 0);
-//	t.transform.localPosition += new Vector3(56f, 3f, 40f);
+		
+	
+	int y = 0;
+	foreach( string cellDescrStr in cellDescriptions ){
+		// Instantiate at position, rotation
+		axisDescr[y] = Instantiate(myPrefab, new Vector3(0.2f, y*1.1f-height, -2 ), Quaternion.identity);
+		//cube.GetComponent<Renderer>().material.color = Random.ColorHSV();
+		axisDescr[y].transform.SetParent(descrHolder);
+		axisDescr[y].name = "descr_"+cellDescrStr;
+		
+		TMP_Text[] descrText = axisDescr[y].GetComponentsInChildren<TMP_Text>();
+		descrText[0].SetText(cellDescrStr);
+		y++;
+	}
+	
 
      }
 }
